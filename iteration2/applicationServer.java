6c9537848dc70b823cd39c69f63bef30d82607ab
andsav13 <andrea13savino@gmail.com>
2024-10-24 19:06:34 +0200

    Update applicationServer.java

--- iteration2/applicationServer.java
+++ iteration2/applicationServer.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 import java.util.Date;
 
 public class applicationServer {
-    private HashMap<String, Application> applications = new HashMap<>();
+    private HashMap<String, Application> PMapplications = new HashMap<>();
+    private HashMap<String, Application> SMapplications = new HashMap<>();
     private HashMap<String, Task> musicSubteamTasks = new HashMap<>();
     private HashMap<String, Task> chefSubteamTasks = new HashMap<>();
     private HashMap<String, String> comments_in = new HashMap<>();
@@ -11,12 +12,17 @@ public class applicationServer {
 
     public Application createApplication(int applicationID, String applicationText, String subteam, ApplicationStatus status, Department department){
         Application newApplication = new Application(applicationID, applicationText, subteam, status, department);
-        applications.put(String.valueOf(applicationID), newApplication);
+        if(department == Department.PRODUCTION){
+            PMapplications.put(String.valueOf(applicationID), newApplication);
+        }
+        else{
+            SMapplications.put(String.valueOf(applicationID), newApplication);
+        }
         return newApplication;
     }
 
     public boolean sendtoMusicSubteam(String applicationID){
-        Application application = applications.get(String.valueOf(applicationID));
+        Application application = PMapplications.get(String.valueOf(applicationID));
         if(application != null){
             String taskID = "TASK" + String.valueOf(applicationID);
             musicSubteamTasks.put(taskID, application);
@@ -27,7 +33,7 @@ public class applicationServer {
     }
 
     public boolean sendtoCefSubteam(String applicationID){
-        Application application = applications.get(String.valueOf(applicationID));
+        Application application = SMapplications.get(String.valueOf(applicationID));
         if(application != null){
             String taskID = "TASK" + String.valueOf(applicationID);
             chefSubteamTasks.put(taskID, application);
@@ -49,7 +55,12 @@ public class applicationServer {
     }
 
     public boolean setApplicationStatus(String applicationID, ApplicationStatus status){
-        Application application = applications.get(String.valueOf(applicationID));
+        if(department == Department.PRODUCTION){
+            Application application = PMapplications.get(String.valueOf(applicationID));
+        }
+        else{
+            Application application = SMapplications.get(String.valueOf(applicationID));
+        }
         if(application != null){
             application.setStatus(status);
             return true;
@@ -63,6 +74,10 @@ public class applicationServer {
 
     public HashMap<String, Task> getMusicSubteamTasks(){
         return musicSubteamTasks;
+    }
+
+    public HashMap<String, Task> getChefSubteamTasks(){
+        return chefSubteamTasks;
     }
 
     public HashMap<String, String> getComments_in(){
