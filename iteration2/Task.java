78159bb4c67647eeb11040b75f76b16e9eff6c72
andsav13 <andrea13savino@gmail.com>
2024-10-24 18:05:17 +0200

    Update Task.java

--- iteration2/Task.java
+++ iteration2/Task.java
@@ -3,17 +3,18 @@ package miniproject;
 public class Task {
     private String taskID;
     private Application application;
-}   
+   
 
-public Task(String taskID, Application application){
-    this.taskID = taskID;
-    this.application = application;
-}
+    public Task(String taskID, Application application){
+        this.taskID = taskID;
+        this.application = application;
+    }
 
-public int getTaskID(){
-    return taskID;
-}
-public Application getApplication(){
-    return application;
+    public int getTaskID(){
+        return taskID;
+    }
+    public Application getApplication(){
+        return application;
+    }   
 }
 
